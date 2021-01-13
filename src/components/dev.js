374d22463d189a52fa1e699219f17d49970626d5
Sebastian <sebastianbednarz1999@gmail.com>
2021-01-13 20:29:23 +0000

    Can pull repo data from github using api now

--- src/components/dev.js
+++ src/components/dev.js
@@ -1,11 +1,41 @@
 import React from 'react'; 
+import { Octokit } from "@octokit/rest";
+
+const projects = getGithubRepos();
+
+async function getGithubRepos() {
+    const octokit = new Octokit();
+
+    let repoList = [];
+
+    await octokit.request('GET /users/{user}/repos',  {
+        user: 'lou429',
+        type: 'public'
+    })
+    .then(({ data }) => {
+        data.forEach(repo => {
+            if(!repo.private)
+            {
+                let repoTemplate = {
+                    id: repo.id,
+                    name: repo.name,
+                    url: repo.url
+                }
+                repoList.push(repoTemplate);
+            }
+        });
+    });
+    console.log("Repo list array: ");
+    console.log(repoList);
+    return repoList; 
+}
 
 function Development() {
     return(
         <div className="container">
             <div className="row">
                 {/* Section for adding github account and socials */}
-                <h1>Row</h1>
+                <h1>Github account</h1>
                 {/* Section to add recent projects, could be done through cards */}
             </div>
         </div>
