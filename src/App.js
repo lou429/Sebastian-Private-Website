fe36753576d710735ad2355624c6ae1224acfa68
Sebastian <sebastianbednarz1999@gmail.com>
2021-01-19 20:24:03 +0000

    Fixed the API loading, now it attaches a callback

--- src/App.js
+++ src/App.js
@@ -1,92 +1,37 @@
 import './App.scss';
 import NavBar from './components/navbar.js';
-import {BrowserRouter as Router, Routes, Route, Link} from '../node_modules/react-router-dom';
+import {BrowserRouter as Router, Routes, Route} from '../node_modules/react-router-dom';
 import Snake from "./components/snake.js";
 import Dev from "./components/dev.js";
 import Home from "./components/home.js";
 import SignInPage from './components/SignInPage.js';
 import User from "./components/UserPage.js";
-import {Octokit} from "@octokit/rest";
-import $ from 'jquery';
+
 import './style.scss';
 
 function App() {
-$(document).ready(function () {
-        const octokit = new Octokit();
-
-        octokit
-            .request('GET /users/{user}/repos', {
-            user: 'lou429',
-            authorization: 'token 75c36c5712b6c0b6cc8d7c8cb834bb9db4ea94a6'
-        })
-            .then(({data}) => {
-                let output = [new GithubRepos()];
-                data.forEach((repo) => {
-                    output.push(new GithubRepos(repo.id, repo.name, repo.html_url, repo.owner.login, repo.owner.url, repo.created_at.substring(0, 10), repo.tags_url));
-                })
-                $('#repoCount').text(data.length);
-                repoList = receivedGithubCallback(output);
-                console.log("FINAL");
-                console.log(repoList);;
-            })
-            .catch(({exception}) => {
-                console.log(exception);
-                // alert("Error occurred when loading repositories");
-            });
-    });
-
-function receivedGithubCallback(data) {
-    let counter = 0;
-    let localList = [new GithubRepos()];
-    data.forEach(cardInfo => {
-        localList.push(cardInfo);
-        // console.log(cardInfo);
-        counter++;
-    });
-
-    $('#repoCount').text(counter);
-    return localList;
-}
-
-let repoList;
-
-return (
-    <div className="App">
-        <NavBar/>
-        <br/>
-        <br/>
-        <div id="content-wrapper" className="base-content-container">
-            <div className="base-content-row">
-                <Router>
-                    <Routes>
-                        <Route path="/" element={< Home />} exact/>
-                        <Route path="/home" element={< Home />}/>
-                        <Route
-                            path="/dev"
-                            element={< Dev repoList = {
-                            repoList
-                        } />}/>
-                        <Route path="/SignIn" element={< SignInPage />}/>
-                        <Route path="/User" element={< User />}/>
-                    </Routes>
-                </Router>
-                <br/>
+    return (
+        <div className="App">
+            <NavBar/>
+            <br/>
+            <br/>
+            <div id="content-wrapper" className="base-content-container">
+                <div className="base-content-row">
+                    <Router>
+                        <Routes>
+                            <Route path="/" element={< Home />} exact/>
+                            <Route path="/home" element={< Home />}/>
+                            <Route
+                                path="/dev"
+                                element={< Dev />}/>
+                            <Route path="/SignIn" element={< SignInPage />}/>
+                            <Route path="/User" element={< User />}/>
+                        </Routes>
+                    </Router>
+                </div>
             </div>
         </div>
-    </div>
-);
-}
-
-class GithubRepos {
-    constructor(id, projectName, projectUrl, creatorName, creatorUrl, date, tagsUrl) {
-        this.id = id;
-        this.projectName = projectName;
-        this.projectUrl = projectUrl;
-        this.creatorName = creatorName;
-        this.creatorUrl = creatorUrl;
-        this.date = date;
-        this.tagsUrl = tagsUrl;
-    }
+    );
 }
 
 export default App;
--- src/components/dev.js
+++ src/components/dev.js
@@ -1,7 +1,34 @@
-import React from 'react';
+import {useState, useEffect} from 'react';
+import {Octokit} from "@octokit/rest";
+import $ from 'jquery';
 import Card from './card';
 
 function Development(props) {
+    const [repoList, setRepoList] = useState([]);
+
+    useEffect(() => {
+        const octokit = new Octokit();
+
+        octokit.request('GET /users/{user}/repos', {
+            user: 'lou429',
+            authorization: 'token 75c36c5712b6c0b6cc8d7c8cb834bb9db4ea94a6'
+        })
+        .then(({data}) => {
+            let output = [new GithubRepos()];
+            data.forEach((repo) => {
+                if(repo.id !== undefined)
+                    output.push(new GithubRepos(repo.id, repo.name, repo.html_url, repo.owner.login, repo.owner.url, repo.created_at.substring(0, 10), repo.tags_url));
+            })
+            setRepoList(receivedGithubCallback(output));
+            debugger;
+            $("#repoCount").innerHTML = output.length;
+        })
+        .catch(({exception}) => {
+            console.log(exception);
+            setRepoList(null);
+        });
+    }, [])
+
     return (
         <div className="container">
             <div className="row">
@@ -17,9 +44,8 @@ function Development(props) {
                 <div className="dev-card-list-container">
                     <div className="dev-card-list">
                         {
-                            props.repoList.map((repo) => {
-                                // <Card date={repo.date} projectName={repo.projectName} projectUrl={repo.projectUrl} creatorName={repo.creatorName} creatorUrl={repo.creatorUrl} tagsUrl={repo.tagsUrl}/>
-                                <Card {...repo} />
+                            repoList.forEach((repo) => {
+                                <Card {...repo}/>
                             })
                         }
                         {/* <Card date="01-01-2020" projectName="Non est veniam laboris" creatorName="Sebastian Bednarz" creatorUrl="https://github.com/raegar"/>
@@ -33,6 +59,29 @@ function Development(props) {
             </div>
         </div>
     );
+}
+
+function receivedGithubCallback(data) {
+    let counter = 0;
+    let localList = [new GithubRepos()];
+    data.forEach(cardInfo => {
+        localList.push(cardInfo);
+    });
+
+    $('#repoCount').text(counter);
+    return localList;
+}
+
+class GithubRepos {
+    constructor(id, projectName, projectUrl, creatorName, creatorUrl, date, tagsUrl) {
+        this.id = id;
+        this.projectName = projectName;
+        this.projectUrl = projectUrl;
+        this.creatorName = creatorName;
+        this.creatorUrl = creatorUrl;
+        this.date = date;
+        this.tagsUrl = tagsUrl;
+    }
 }
 
 export default Development;
