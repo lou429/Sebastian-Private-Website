b824bd4d05bbdc1bd5d970d72baa1bab80f40c0d
Sebastian <sebastianbednarz1999@gmail.com>
2021-01-20 01:52:55 +0000

    Started loading tag data from github api

--- src/components/card.js
+++ src/components/card.js
@@ -1,70 +1,100 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './card.scss';
-import { Octokit } from "@octokit/rest";
+import {Octokit} from "@octokit/rest";
 import $ from 'jquery';
-// function Card({id, name, url, date}) {
-
 
 function Card(props) {
-    // async function getGithubTags(tagsUrl) {
-    //     let tagList = [{tagName: ""}];
-    //     tagList.pop();
-
-    //     const octokit = new Octokit();
-    //     await octokit.request('GET /users/{user}/repos', {
-    //         user: ''
-    //     })
-    //     .then(({ data }) => {
-    //         data.forEach((tags) => {
-    //             tagList.push(tags);
-    //         })
-    //     })
-    //     .catch(({ exception }) => {
-    //         console.log(exception);
-    //     });
-
-    //     return tagList;
-    // }
-    
-    $(document).ready(function() {
-        $('.dev-card-avatar-image').each(function() {
-            if($(this).attr('src') === 'undefined.png')
-                $(this).attr('class', 'dev-card-avatar-image-no-image');
-        });
-        // TODO: Remove this once a better way to load repo data 
-        // $('#repoCount').text(counter);
+    const [tagList,
+        setTagList] = useState([]);
+    $(document).ready(function () {
+        $('.dev-card-avatar-image')
+            .each(function () {
+                if ($(this).attr('src') === 'undefined.png') 
+                    $(this).attr('class', 'dev-card-avatar-image-no-image');
+                }
+            );
     })
+
+    useEffect(function loadTagInfo() {
+        async function getData() {
+            const octokit = new Octokit();
+
+            await octokit.request('GET /repos/{user}/{repoName}/languages', {
+                user: 'lou429',
+                repoName: props.projectName === undefined
+                    ? "Could not get name"
+                    : props
+                        .projectName
+                        .replace(" ", ""),
+                authorization: 'token 75c36c5712b6c0b6cc8d7c8cb834bb9db4ea94a6'
+            }).then(({data}) => {
+                setTagList(receivedTagCallback(data));
+                console.log(data);
+            }).catch(({exception}) => {
+                console.log(exception);
+                setTagList(null);
+            });
+        }
+        getData();
+        console.log(tagList);
+    }, [])
 
     return (
         <div className="dev-card">
             <div className="dev-card-header">
                 <p>{props.date || "01-01-2000"}</p>
-                <h2>{props.projectName || "Could not get name"}</h2>
+                <h2
+                    className={props.projectName == ''
+                    ? 'card-profile-failed'
+                    : 'card-profile-active'}>{props.projectName || "Could not get name"}</h2>
             </div>
 
             <div className="dev-card-body">
-                <a className="dev-card-avatar-link" href={props.url} target="_blank" rel="noopener noreferrer">
-                    <img className="dev-card-avatar-image" src={props.creatorUrl + ".png"} alt="Author avatar"/>
+                <a
+                    className="dev-card-avatar-link"
+                    href={props.url}
+                    target="_blank"
+                    rel="noopener noreferrer">
+                    <img
+                        className="dev-card-avatar-image"
+                        src={props.creatorUrl + ".png"}
+                        alt="Author avatar"/>
                 </a>
                 <svg className="dev-card-half-circle" viewBox="0 0 106 57">
                     <path d="M102 4c0 27.1-21.9 49-49 49S4 31.1 4 4"></path>
                 </svg>
                 <div className="dev-card-author-name">
                     <div className="dev-card-author-name-prefix">Author</div>
-                    <a id="authorName" href={props.creatorUrl} target="_blank" rel="noopener noreferrer">{props.creatorName || "Could not get name"}</a>
+                    <a
+                        id="authorName"
+                        href={props.creatorUrl}
+                        target="_blank"
+                        rel="noopener noreferrer">{props.creatorName || "Could not get name"}</a>
                 </div>
             </div>
             <div className="dev-card-tags">
-                <a>{props.tagsUrl}</a>
-                    {/* TODO: Return coding tag from github */}
-                    {
-                        // getGithubTags(props.tagsUrl).map((tag) => {
-                        //     <a href={props.projectUrl  || "#"} target="_blank" rel="noopener noreferrer">{tag} </a>
-                        // })
-                    }
-                </div>
+                {/* {tagList.map(tag => (
+                    <a>{tag}</a>
+                ))} */}
+                {/* Object.keys(tagList).map(tag => (     <a>{tag}</a> ))*/}
+            </div>
         </div>
     );
 }
+
+function receivedTagCallback(data) {
+    debugger;
+    console.log(data);
+    if (typeof(data) === typeof(Array)) {
+        let localList = [];
+        data.forEach((tagInfo) => {
+            if (tagInfo !== 'undefined' && tagInfo !== undefined) 
+                localList.push(tagInfo);
+            }
+        );
+        return localList;
+    } else 
+        return Object.keys(data);
+    }
 
 export default Card;
--- src/components/dev.js
+++ src/components/dev.js
@@ -2,31 +2,31 @@ import {useState, useEffect} from 'react';
 import {Octokit} from "@octokit/rest";
 import $ from 'jquery';
 import Card from './card';
+import './dev.scss';
 
 function Development(props) {
-    const [repoList, setRepoList] = useState([]);
+    const [repoList,
+        setRepoList] = useState([]);
 
-    useEffect(() => {
-        const octokit = new Octokit();
+    useEffect(function getApiData() {
+        async function getData() {
+            const octokit = new Octokit();
 
-        octokit.request('GET /users/{user}/repos', {
-            user: 'lou429',
-            authorization: 'token 75c36c5712b6c0b6cc8d7c8cb834bb9db4ea94a6'
-        })
-        .then(({data}) => {
-            let output = [new GithubRepos()];
-            data.forEach((repo) => {
-                if(repo.id !== undefined)
-                    output.push(new GithubRepos(repo.id, repo.name, repo.html_url, repo.owner.login, repo.owner.url, repo.created_at.substring(0, 10), repo.tags_url));
+            await octokit.request('GET /users/{user}/repos', {
+                user: 'lou429',
+                authorization: 'token 75c36c5712b6c0b6cc8d7c8cb834bb9db4ea94a6'
             })
-            setRepoList(receivedGithubCallback(output));
-            debugger;
-            $("#repoCount").innerHTML = output.length;
-        })
-        .catch(({exception}) => {
-            console.log(exception);
-            setRepoList(null);
-        });
+            .then(({data}) => {
+                setRepoList(receivedGithubCallback(data));
+                $("#repoCount").text(repoList.length);
+            })
+            .catch(({exception}) => {
+                console.log(exception);
+                setRepoList(null);
+            });
+        }
+
+        getData(); 
     }, [])
 
     return (
@@ -37,23 +37,17 @@ function Development(props) {
                 {/* Section to add recent projects, could be done through cards */}
             </div>
             <div className="row">
-                <h1>Repos: 
-                    <i id="repoCount"></i>
+                <h1>Repos:
+                    <a
+                        id="repoCount"
+                        alt="Repo count"
+                        href="https://github.com/lou429?tab=repositories"
+                        target="_blank"
+                        rel="noopener noreferrer">0</a>
                 </h1>
-                <br/>
                 <div className="dev-card-list-container">
                     <div className="dev-card-list">
-                        {
-                            repoList.forEach((repo) => {
-                                <Card {...repo}/>
-                            })
-                        }
-                        {/* <Card date="01-01-2020" projectName="Non est veniam laboris" creatorName="Sebastian Bednarz" creatorUrl="https://github.com/raegar"/>
-                        <Card date="01-01-2020" projectName="Non est veniam laboris" creatorName="Sebastian Bednarz"/>
-                        <Card date="01-01-2020" projectName="Non est veniam laboris" creatorName="Sebastian Bednarz"/>
-                        <Card date="01-01-2020" projectName="Non est veniam laboris" creatorName="Sebastian Bednarz"/>
-                        <Card date="01-01-2020" projectName="Non est veniam laboris" creatorName="Sebastian Bednarz"/>
-                        <Card date="01-01-2020" projectName="Non est veniam laboris" creatorName="Sebastian Bednarz"/> */}
+                        {repoList.map(repo => (<Card {...repo}/>))}
                     </div>
                 </div>
             </div>
@@ -62,13 +56,10 @@ function Development(props) {
 }
 
 function receivedGithubCallback(data) {
-    let counter = 0;
     let localList = [new GithubRepos()];
     data.forEach(cardInfo => {
         localList.push(cardInfo);
     });
-
-    $('#repoCount').text(counter);
     return localList;
 }
 
