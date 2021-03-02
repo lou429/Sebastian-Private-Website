f60d0b1a1c8c1b887ffd9da58447ea6c78e50ca0
Sebastian <sebastianbednarz1999@gmail.com>
2021-03-02 00:31:21 +0000

    Removed old navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -3,59 +3,21 @@ import NavLink from './navlink.js';
 import $ from 'jquery';
 import './navbar.scss';
 
-// $(document).ready(function() {
-//     $('.nav-link-source').each(function(index, obj) {
-//         if($(obj).attr("href") === document.URL.split('/')[1])
-//             $(this).attr('class', "nav-link-source-active");
-//     })
-// });
-
-$(document).ready(function() {
-    let url = (document.URL);
-    url = "/" + url.split('/')[3];
-    $('.nav-link-body').each(function() {
-        let modClass = (url === $(this.childNodes[0]).attr('href') ? "-active" : '');
-        let node = this.childNodes[0];
-        $(node).attr('class', 'nav-link-src' + modClass);
-        if(modClass === '') {
-            $(node).hover(function() {
-                // $('.nav-link-src-active').css('font-size', '24px');
-            });
-            $(node).mouseleave(function() {
-                // $('.nav-link-src-active').css('font-size', '28px');
-            })
-        }
-        else {
-            $(node).hover(function() {
-                // $('.nav-link-src-active').css('font-size', '24px');
-            });
-            $(node).mouseleave(function() {
-                // $('.nav-link-src-active').css('font-size', '28px');
-            })
-        }
-    });
-});
-
 function NavBar() {
     return (
-        <div className="navbar">
+        <div className="navbar-container">
+            <div className="navbar-logo-container">
+                <div className="content-end-container">
+                    <a href="https://github.com/lou429" target="_blank" rel="noopener noreferrer">
+                        <img id="github-profile" src="https://github.com/lou429.png" alt="Github profile" height="40px" width="40px"/>
+                    </a>
+                </div>
+            </div>
             <div className="nav-content-container">
                 <NavLink href="/">Home</NavLink>
                 <NavLink href="/dev">Development</NavLink>
                 <NavLink href="/user">About me</NavLink>
                 <NavLink href="/contact">Contact me</NavLink>
-            </div>
-            <div className="content-end">
-                <div className="content-end-container">
-                    <a href="https://github.com/lou429" target="_blank" rel="noopener noreferrer">
-                        <img
-                            id="github-profile"
-                            src="https://github.com/lou429.png"
-                            alt="Github profile"
-                            height="40px"
-                            width="40px"/>
-                    </a>
-                </div>
             </div>
         </div>
     );
