895b6a24d744efd3c73518d8d8d0bd778a099011
Sebastian <sebastianbednarz1999@gmail.com>
2021-03-09 22:29:14 +0000

    Added content wrapper to no page found

--- src/components/NoRouteFound.js
+++ src/components/NoRouteFound.js
@@ -1,10 +1,11 @@
 import React from 'react'; 
+import ContentWrapper from './contentwrapper';
 
 function NoPage() {
     return(
-        <>
+        <ContentWrapper heading="No page">
             <h1>Page {window.location.href} not found</h1>
-        </>
+        </ContentWrapper>
     );
 
 }
